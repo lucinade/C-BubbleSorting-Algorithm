4fa84faf3433564e9eaaae27b0b73f26c724605b
lucinade <88952626+lucinade@users.noreply.github.com>
2021-09-06 16:31:44 -0700

    Add files via upload

---
+++ bsorting.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void SortArray(int arr[], int n);
+
+int main(void) {
+
+	int arr[] = {5,1,2,1,99,11,100,3};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int ccc[n];
+	
+	SortArray(arr, n);
+
+	return 0;
+}
+/* Take in arr and organize it */
+void SortArray(int arr[], int n)
+{
+		
+	int i, j;
+	
+	for (i = 0; i < n-1; ++i)
+		for (j = 0; j < n-i-1; ++j)
+		{
+			if (arr[j] > arr[j+1]) {
+				int t = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = t;
+
+			}
+}
+	for (i=0;i<=n-1;++i)
+		printf("%d\n", arr[i]);
+}
